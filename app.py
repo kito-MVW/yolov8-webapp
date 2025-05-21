f3e123274e33f4f3c704b85e123a1163021ba33c
kito-MVW <77981438+kito-MVW@users.noreply.github.com>
2025-05-21 23:21:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 app = Flask(__name__)
 
 # โหลด YOLOv8 โมเดล
-model = YOLO("best.pt")
+model = YOLO("model/best.pt")
 
 # โฟลเดอร์สำหรับเก็บภาพที่อัปโหลดและผลลัพธ์
 UPLOAD_FOLDER = os.path.join('static', 'uploads')
